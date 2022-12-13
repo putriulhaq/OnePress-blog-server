f1e7ce25cde6b76e24dacd2c823798a44b340ec0
reyvidod <reyvidod@gmail.com>
2022-12-13 23:54:10 +0700

    change port to 3001 so it won't have conflict with the front end, change mongodb connection to rey's mongo atlas db

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 const {about} = require('./router')
-const port = 3000;
+const port = 3001;
 
 app.use(cors())
 app.use(express.json());
--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
 
-  mongoose.connect('mongodb://localhost:27017/usersdb',
+  mongoose.connect('mongodb+srv://<username>:<password>@scc.3tv5tm0.mongodb.net/?retryWrites=true&w=majority',
     {
       useNewUrlParser: true,
       // useFindAndModify: false,
