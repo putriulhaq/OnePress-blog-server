36494a6ffc0efb8ebe84f9dfa35d9a0ee69d0423
putriulhaq0609@gmail.com <putriulhaq0609@gmail.com>
2022-12-13 23:40:34 +0700

    test routes

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
-const connect = require("./database/db");
+const connection = require("./models/index");
+const {about} = require("./router")
 
 //database connection
-connect();
+connection();
 
 //middelwares
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.port || 3000;
+
+app.use("/", [about]);
 app.listen(port, () => {
   console.log(port, "Server is open with port!");
 });
