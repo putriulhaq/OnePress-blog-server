bb2115ebbd003c5aef581cb288112d677f6a97c1
reyvidod <reyvidod@gmail.com>
2022-12-14 23:05:07 +0700

    add schema, put mongodb url on .env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
-const connection = require("./models/index");
+const connection = require("./index");
 const {about, posts} = require("./router")
 
 //database connection
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
 
-  mongoose.connect('mongodb+srv://reyvidod:rRey27AUG03_@scc.3tv5tm0.mongodb.net/?retryWrites=true&w=majority',
+  mongoose.connect(process.env.MONGO_URL,
     {
       useNewUrlParser: true,
       // useFindAndModify: false,
