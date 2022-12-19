b2ad0d08293dc4954a31f028d4622d2f7942d86d
putriulhaq0609@gmail.com <putriulhaq0609@gmail.com>
2022-12-19 19:53:46 +0700

    connect to mongoose

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const dbname = 'onepress'
 const connect = () => {
 
-    mongoose.connect('mongodb://localhost:27017/onepress',
+    // mongoose.connect('mongodb://localhost:27017/onepress',
+    mongoose.connect(`mongodb+srv://putriulhaq:Bismillahsukses06@cluster0.b4p9mwv.mongodb.net/${dbname}?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         // useFindAndModify: false,
