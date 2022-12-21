cc5b28dca50a6cd9cbf3b19b1e511ced59049fe8
reyvido <reyvidod@gmail.com>
2022-12-21 16:38:34 +0700

    change categories to string, add post blog

--- router/posts.js
+++ router/posts.js
@@ -2,6 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Posts = require("../models/Post");
 
+router.post("/posts", async (req, res) => {
+  const newPost = new Posts(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/posts/", async (req, res) => {
   const username = req.query.user;
   const catName = req.query.cat;
