8c916fc492d91e973c297ce3bc0d8800d920b00b
putriulhaq0609@gmail.com <putriulhaq0609@gmail.com>
2022-12-21 22:29:14 +0700

    db aku ulhaq

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dbname = 'onepress'
 const connect = () => {
-    mongoose.connect(process.env.MONGO_URL,
+    mongoose.connect(process.env.MONGO_URL_2,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true
